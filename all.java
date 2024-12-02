4f26d377bc0006240d9e7699171d55ec240963da
Suvetha-Arivazhagan <suvethaarivazhagan04@gmail.com>
2024-12-02 20:14:38 +0530

    Add files via upload

---
+++ all.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+	    Scanner sc=new Scanner(System.in);
+	    int i=sc.nextInt();
+	    String s=sc.next();
+	    char c=sc.next().charAt(0);
+	    
+	    System.out.println(i);
+	    System.out.println(s);
+	    System.out.println(c);
+	    
+
+	}
+}
